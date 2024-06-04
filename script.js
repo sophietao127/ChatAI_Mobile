755f94b4d7861d159d5d71ec2ae07593132a4e6f
sophietao127 <sophietao127@gmail.com>
2024-06-03 18:45:06 -0700

    add readline to get user input

--- script.js
+++ script.js
@@ -3,35 +3,48 @@ config();
 // console.log(process.env.API_KEY);
 
 import OpenAI from "openai";
+import readline from "readline";
 
 const openai = new OpenAI({
   apiKey: process.env.API_KEY,
 });
 // const openai = new OpenAI();
 
-// async function main() {
-//   const completion = await openai.chat.completions.create({
-//     messages: [{ role: "system", content: "You are a helpful assistant." }],
-//     model: "gpt-3.5-turbo",
-//   });
+const userInterface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-//   console.log(completion.choices[0]);
-// }
-
-// main();
-async function main() {
+userInterface.prompt();
+userInterface.on("line", async (input) => {
   const completion = await openai.chat.completions.create({
     messages: [
       {
         role: "system",
         content: "You are a helpful assistant designed to output JSON.",
       },
-      { role: "user", content: "Who won the world series in 2016?" },
+      { role: "user", content: input },
     ],
     model: "gpt-3.5-turbo-0125",
     response_format: { type: "json_object" },
   });
   console.log(completion.choices[0].message.content);
-}
+  userInterface.prompt();
+});
 
-main();
+// async function main() {
+//   const completion = await openai.chat.completions.create({
+//     messages: [
+//       {
+//         role: "system",
+//         content: "You are a helpful assistant designed to output JSON.",
+//       },
+//       { role: "user", content: "Who won the world series in 2016?" },
+//     ],
+//     model: "gpt-3.5-turbo-0125",
+//     response_format: { type: "json_object" },
+//   });
+//   console.log(completion.choices[0].message.content);
+// }
+
+// main();
